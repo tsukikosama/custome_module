d15d041bee8845a0eb343ac1cac04928da2c4a0a
weilai <1083344129@qq.com>
2026-05-14 17:41:44 +0800

    [fix] 发起通知全体用户

--- continew-system/src/main/java/top/continew/admin/system/listener/SendBroadcastMessageEventListener.java
+++ continew-system/src/main/java/top/continew/admin/system/listener/SendBroadcastMessageEventListener.java
@@ -65,7 +65,7 @@ public class SendBroadcastMessageEventListener {
             msg.setText(text);
 
             sendMessageReq.setMsg(msg);
-
+            sendMessageReq.setToAllUser(true);
             // 发送钉钉会话消息（全体用户）
             dingTalkApiService.sendConversationMessage(sendMessageReq,event.isFlag());
             log.info("发送广播消息钉钉推送成功：内容={}", content);
